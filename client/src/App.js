9306020e4486edd9e3cc7dfde76c96cb5c47aa2e
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-27 22:22:37 +0300

    implemented login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import  Signup  from "./Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Login';
+import Navbar  from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/join-realtor.com" element={<Signup />} />
+
+          </Routes>
+      </Router>
     </div>
   );
 }
