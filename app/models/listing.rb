fa86256e6851ebd9702012224db2f4a65b146d5f
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-01 16:23:33 +0300

    added validation for property type attribute for a listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
     belongs_to :user
     validates :address, presence:true, uniqueness: true
-    validates :property_type, presence:true, inclusion: {in: ["Sell", "Rent"]}
+    validates :property_type, presence:true, inclusion: {in: ["Sell", "Rent", "Lease"]}
     validates :area_in_sqm, presence:true
     validates :beds, presence:true
     validates :baths, presence:true
