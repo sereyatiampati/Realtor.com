21dad1c4a867755984c74277748c6bd717d2b85e
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-01 16:39:18 +0300

    added a conditional rendering for displaying a user properties page

--- client/src/components/MyProperties.js
+++ client/src/components/MyProperties.js
@@ -1,27 +1,42 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import EachListing from "./EachListing";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
-function MyProperties({user}) {
+function MyProperties() {
     const [myListings, setMyListings] = useState([]);
+    const [item, setItem]= useState(true)
+    const navigate = useNavigate()
 
     useEffect(() => {
       fetch("/my-listings")
         .then((r) => r.json())
         .then(setMyListings);
-    }, []);
+    }, [item]);
+
+    function onDelete() {
+      setItem(!item)
+    }
+
+
     return(
         <>
         <div id="property-carousel" class="swiper">
           <div class="swiper-wrapper"  style={{display: 'flex', flexWrap: 'wrap', width: '100%', height: '100%', flexDirection: 'row', paddingRight: '0', justifyContent: 'center'}} >
             {
-                myListings.length > 0 ? (myListings.map((listing) => <EachListing key={listing.id} listing={listing}/>)
+                myListings.length > 0 ? (
+                  <div className='container'>
+                    <h3>Do you have another property that you want listed to Sell,  Rent or Lease out?</h3>
+                <Button variant="outline-primary" onClick={()=> navigate("/property/new")} style={{width: "100px", height: "70px"}}>
+                   Add Listing
+                </Button>
+                  <div style={{display: "flex", flexWrap: 'wrap'}}>{myListings.map((listing) => <EachListing key={listing.id} listing={listing} onDelete={onDelete}/>)}</div>
+                  </div>
                 ) : (
             <div className='container'>
                 <h2>No Properties Found</h2>
                 
-                <Button variant="outline-primary" as={Link} to="/add-property">
+                <Button variant="outline-primary" as={Link} to="/property/new">
                    Add Listing
                 </Button>
 
