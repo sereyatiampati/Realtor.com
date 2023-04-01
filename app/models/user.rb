199b1cc50fe898b88b5fff21828893fd4199aecb
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-01 16:26:59 +0300

    added a dependent destroy on user model to detroy the asccociated objects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :listings
+    has_many :listings,  dependent: :destroy
     validates :email, presence:true, uniqueness:true
     validates :username, presence:true, uniqueness:true, length: { minimum: 3 }
     validates :password, presence: true, length: { minimum: 3 }
